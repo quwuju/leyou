7bb7f6e042924f181932e39a5b87bcac7e79dcda
quwuju <wuju_qu@168.com>
2020-02-07 20:41:55 +0800

    leyou后台第一次提交

---
+++ leyou/leyou-authentication/leyou-authentication-common/src/test/java/com/leyou/auth/test/JwtTest.java
@@ -0,0 +1,60 @@
+package com.leyou.auth.test;
+
+import com.leyou.auth.entity.UserInfo;
+import com.leyou.auth.utils.JwtUtils;
+import com.leyou.auth.utils.RsaUtils;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.util.Date;
+
+/**
+ * @Author: 98050
+ * @Time: 2018-10-23 20:58
+ * @Feature: JWT测试
+ */
+public class JwtTest {
+
+    private static final String pubKeyPath = "E:\\tmp\\rsa\\rsa.pub";
+
+    private static final String priKeyPath = "E:\\tmp\\rsa\\rsa.pri";
+
+    private PublicKey publicKey;
+
+    private PrivateKey privateKey;
+
+    @Test
+    public void testRsa() throws Exception {
+        RsaUtils.generateKey(pubKeyPath, priKeyPath, "234");
+    }
+
+    @Before
+    public void testGetRsa() throws Exception {
+        this.publicKey = RsaUtils.getPublicKey(pubKeyPath);
+        this.privateKey = RsaUtils.getPrivateKey(priKeyPath);
+    }
+
+    @Test
+    public void testGenerateToken() throws Exception {
+        // 生成token
+        String token = JwtUtils.generateToken(new UserInfo(20L, "jack"), privateKey, 5);
+        System.out.println("token = " + token);
+    }
+
+    @Test
+    public void testParseToken() throws Exception {
+        String token = "eyJhbGciOiJSUzI1NiJ9.eyJpZCI6MjAsInVzZXJuYW1lIjoiamFjayIsImV4cCI6MTU4MTUwNjE4MH0.H60WXItoQ_nMNnEgZQqID8_basmBZvZM1WPBw05u-84cqT62yI4oP9oB2w3bTqK4XyBaIn-tKDMK06kbonNKZMOHC7IALuSi9NmcZG1V6eQM5d6xn5YYr_Q_aOh6psFyu0iAkaNgHMx7Srjk9wYhWq1uTq8BkwBkSR4fqzqVeMY";
+
+        // 解析token
+        UserInfo user = JwtUtils.getInfoFromToken(token, publicKey);
+        System.out.println("id: " + user.getId());
+        System.out.println("userName: " + user.getUsername());
+    }
+
+    @Test
+    public void date(){
+        System.out.println(new Date());
+    }
+}
